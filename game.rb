bbb832e29b253b66a604acf2902af027c9e412d3
Andrea <andreamayhirji@gmail.com>
2018-11-07 01:29:09 +0000

    Added the ask_question method

--- game.rb
+++ game.rb
@@ -7,28 +7,36 @@ class Game
     # The game needs to start
     # The game requires two players to begin
     # The game ends when a player's score == 0
-
     # The game would contain the loop
 
+    attr_reader :P1, :P2 
+
     def initialize(player1, player2)
-        @P1 = Player.new
-        @P2 = Player.new
+        @P1 = player1
+        @P2 = player2
         @players = [@P1, @P2]
     end
 
     def start
+        puts "Welcome to the game, #{@P1} and #{@P2}"
+        # runs your game loop
         @players.cycle do |player| 
-            # runs your game loop
             # every loop =>
-                # asks a user a question => method from question classs
+            puts 'Ask a question'                
+            # asks a user a question => method from question classs
+            question = Question.new
+            question.ask_question()
                 # accepts a answer => gets.chomp
                 # checks to see if answer is right => method from question class that accepts gets.chomp 
                 # decrements player lives if wrong => method in your player class
                 # checks to see if game should continue (player has more than 0 lives) => if statement in your start
+        break
         end
     end
 
 end
 
-game = Game.new
+game = Game.new("Andrea", "Matthew")
 game.start
+# puts game.P1
+# puts game.P2
--- question.rb
+++ question.rb
@@ -2,17 +2,21 @@ require './score.rb'
 
 class Question
     # A new math question is generated for each turn by picking two numbers between 1 and 20
-    def initialize(num1, num2)
-        @num1 = num1
-        @num2 = num2
-    end
-
+    
     def ask_question #public method?
-        # num1 + num2 = answer
+        prng = Random.new
+        num1 = prng.rand(20)
+        num2 = prng.rand(20)
+        puts "What does #{num1} + #{num2} equal?"
+        # maybe I don't do this here....
         # if answer is false score -= 1 puts "wrong"
         # else answer is true puts "correct"
     end
     
 end
+
+
+# question1 = Question.new
+# question1.ask_question
 
 
